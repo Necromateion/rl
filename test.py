9b6f5485cf8b6cb116987db16ee89140ffe67b93
andy <andy.kiuchi@epita.fr>
2023-11-18 19:26:00 +0100

    before

--- test.py
+++ test.py
@@ -12,15 +12,32 @@ from random import sample, random
 import cv2
 from random import randint
 
+MINIBATCH_SIZE = 32
+REPLAY_REPLAY_MEMORY_SIZE = 1000000
+AGENT_HISTORY_LENGTH = 4
+TARGET_NETWORK_UPDATE_FREQUENCY = 10000
+DISCOUNT_FACTOR = 0.99
+ACTION_REPEAT = 4
+UPDATE_FREQUENCY = 4
+LEARNING_RATE = 0.00025
+GRADIENT_MOMENTUM = 0.95
+SQUARED_GRADIENT_MOMENTUM = 0.95
+MIN_SQUARED_GRADIENT = 0.01
+INITIAL_EXPLORATION = 1
+FINAL_EXPLORATION = 0.1
+FINAL_EXPLORATION_FRAME = 1000000
+REPLAY_START_SIZE = 50000
+NO_OP_MAX = 30
+
 class ConvModel(nn.Module):
     def __init__(self, num_actions):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=4, out_channels=32, kernel_size=8, stride=4)
+        self.conv1 = nn.Conv2d(in_channels=AGENT_HISTORY_LENGTH, out_channels=32, kernel_size=8, stride=4)
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
         self.fc1 = nn.Linear(7 * 7 * 64, 512)  
         self.fc2 = nn.Linear(512, num_actions)
-        self.opt = optim.Adam(self.parameters(), lr=0.00025)
+        self.opt = optim.Adam(self.parameters(), LEARNING_RATE)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -30,12 +47,7 @@ class ConvModel(nn.Module):
         x = F.relu(self.fc1(x))
         return self.fc2(x)
 
-def setup_frame_stacking_and_resizing(env, w, h, num_stack=4):
-    buffer = np.zeros((num_stack, h, w), 'uint8')
-    frame = None
-    return env, buffer, frame, w, h, num_stack
-
-def preprocess_frame(max_frame, w, h):
+def preprocess_frame(max_frame):
     Luminance_frame = rgb_to_luminance(max_frame).astype(np.uint8)
     resized_frame = cv2.resize(Luminance_frame, dsize=(84, 84))
     return resized_frame
@@ -43,25 +55,18 @@ def preprocess_frame(max_frame, w, h):
 def rgb_to_luminance(frame):
     return 0.299 * frame[:, :, 0] + 0.587 * frame[:, :, 1] + 0.114 * frame[:, :, 2]
 
-def step_frame_stack(env, action, buffer, w, h, num_stack):
-    im, reward, done, info, _ = env.step(action)
-    frame = im.copy()
-    im = preprocess_frame(im, w, h)
-    buffer[1:num_stack, :, :] = buffer[0:num_stack-1, :, :]
+def step_buffer_stack(env, action, buffer):
+    im, reward, done, _, _ = env.step(action)
+    im = preprocess_frame(im)
+    buffer[1:4, :, :] = buffer[0:4-1, :, :]
     buffer[0, :, :] = im
-    return buffer.copy(), reward, done, info, frame
+    return buffer.copy(), reward, done
 
-def render_frame_stack(env, mode, frame):
-    if mode == 'rgb_array':
-        return frame
-    return env.render(mode)
-
-def reset_frame_stack(env, buffer, w, h, num_stack):
+def reset_frame_stack(env, buffer):
     im, _ = env.reset()
-    frame = im.copy()
-    im = preprocess_frame(im, w, h)
-    buffer = np.stack([im]*num_stack, 0)
-    return buffer.copy(), frame
+    im = preprocess_frame(im)
+    buffer = np.stack([im]*4, 0)
+    return buffer.copy()
 
 @dataclass
 class Sarsd:
@@ -71,9 +76,8 @@ class Sarsd:
     next_state: Any
     done: bool
 
-
 class ReplayBuffer:
-    def __init__(self, buffer_size=100000):
+    def __init__(self, buffer_size=REPLAY_REPLAY_MEMORY_SIZE):
         self.buffer_size = buffer_size
         self.buffer = [None] * buffer_size
         self.idx = 0
@@ -91,82 +95,36 @@ class ReplayBuffer:
 def update_tgt_model(m, tgt):
     tgt.load_state_dict(m.state_dict())
 
-def train_step(model, state_transitions, tgt, num_actions, device, gamma=0.99):
-    cur_states = torch.stack(([torch.Tensor(s.state) for s in state_transitions])).to(
-        device
-    )
-    rewards = torch.stack(([torch.Tensor([s.reward]) for s in state_transitions])).to(
-        device
-    )
-    mask = torch.stack(
-        (
-            [
-                torch.Tensor([0]) if s.done else torch.Tensor([1])
-                for s in state_transitions
-            ]
-        )
-    ).to(device)
-    next_states = torch.stack(
-        ([torch.Tensor(s.next_state) for s in state_transitions])
-    ).to(device)
+def train_step(model, state_transitions, tgt, num_actions, device, gamma=DISCOUNT_FACTOR):
+    cur_states = torch.stack(([torch.Tensor(s.state) for s in state_transitions])).to(device)
+    rewards = torch.stack(([torch.Tensor([s.reward]) for s in state_transitions])).to(device)
+    next_states = torch.stack(([torch.Tensor(s.next_state) for s in state_transitions])).to(device)
+    mask = torch.stack(([torch.Tensor([0]) if s.done else torch.Tensor([1])for s in state_transitions])).to(device)    
     actions = [s.action for s in state_transitions]
 
     with torch.no_grad():
-        qvals_next = tgt(next_states).max(-1)[0]  # (N, num_actions)
+        qvals_next = tgt(next_states).max(-1)[0]  
 
     model.opt.zero_grad()
-    qvals = model(cur_states)  # (N, num_actions)
+    qvals = model(cur_states)  
     one_hot_actions = F.one_hot(torch.LongTensor(actions), num_actions).to(device)
 
     loss_fn = nn.SmoothL1Loss()
-    loss = loss_fn(
-        torch.sum(qvals * one_hot_actions, -1), rewards.squeeze() + mask[:, 0] * qvals_next * 0.99
-    )
+    loss = loss_fn(torch.sum(qvals * one_hot_actions, -1), rewards.squeeze() + mask[:, 0] * qvals_next * 0.99)
 
     loss.backward()
     model.opt.step()
     return loss
 
-def run_test_episode(model, env, device, max_steps=1000):  # -> reward, movie?
-    frames = []
-    obs = env.reset()
-    frames.append(env.frame)
-
-    idx = 0
-    done = False
-    reward = 0
-    while not done and idx < max_steps:
-        action = model(torch.Tensor(obs).unsqueeze(0).to(device)).max(-1)[-1].item()
-        obs, r, done, _, _ = env.step(action)
-        reward += r
-        frames.append(env.frame)
-        idx += 1
-
-    return reward, np.stack(frames, 0)
-
-def main(test=False, chkpt=None, device="cuda"):
+def main(chkpt=None, device="cuda"):
     do_boltzman_exploration = False
-    memory_size = 1000000
-    min_rb_size = 50000
-    sample_size = 32
-    lr = 0.0001
-
-    # eps_max = 1.0
-    eps_min = 0.1
 
     eps_decay = 0.999999
 
-    env_steps_before_train = 16
-    tgt_model_update = 5000
-    epochs_before_test = 1500
-
     env = gym.make('ALE/Breakout-v5', render_mode='rgb_array')
-    env, buffer, frame, w, h, num_stack = setup_frame_stacking_and_resizing(env, 84, 84, 4)
+    buffer = np.zeros((4, 84, 84), 'uint8')
 
-    test_env = gym.make('ALE/Breakout-v5', render_mode='rgb_array')
-    test_env, test_buffer, test_frame, _, _, _ = setup_frame_stacking_and_resizing(test_env, 84, 84, 4)
-
-    last_observation, last_frame = reset_frame_stack(env, buffer, w, h, num_stack)
+    last_observation = reset_frame_stack(env, buffer)
 
     m = ConvModel(env.action_space.n).to(device)
     if chkpt is not None:
@@ -177,9 +135,8 @@ def main(test=False, chkpt=None, device="cuda"):
     rb = ReplayBuffer()
     steps_since_train = 0
     epochs_since_tgt = 0
-    epochs_since_test = 0
 
-    step_num = -1 * min_rb_size
+    step_num = -1 * REPLAY_START_SIZE
 
     episode_rewards = []
     rolling_reward = 0
@@ -187,25 +144,21 @@ def main(test=False, chkpt=None, device="cuda"):
     tq = tqdm()
     try:
         while True:
-            if test:
-                render_frame_stack(env, 'human', last_frame)
-                time.sleep(0.05)
             tq.update(1)
 
             eps = eps_decay ** (step_num)
-            if test:
-                eps = 0
 
             if do_boltzman_exploration:
                 logits = m(torch.Tensor(last_observation).unsqueeze(0).to(device))[0]
                 action = torch.distributions.Categorical(logits=logits).sample().item()
             else:
                 if random() < eps:
-                    action = env.action_space.sample()  # Random action
+                    action = env.action_space.sample()  
                 else:
                     action = m(torch.Tensor(last_observation).unsqueeze(0).to(device)).max(-1)[-1].item()
 
-            observation, reward, done, info, frame = step_frame_stack(env, action, buffer, w, h, num_stack)
+            observation, reward, done = step_buffer_stack(env, action, buffer)
+            reward = np.clip(reward, -1, 1)
             rolling_reward += reward
 
             rb.insert(Sarsd(last_observation, action, reward, observation, done))
@@ -214,33 +167,25 @@ def main(test=False, chkpt=None, device="cuda"):
 
             if done:
                 episode_rewards.append(rolling_reward)
-                if test:
-                    print(rolling_reward)
                 rolling_reward = 0
-                observation, frame = reset_frame_stack(env, buffer, w, h, num_stack)
+                observation = reset_frame_stack(env, buffer)
 
             steps_since_train += 1
             step_num += 1
 
             if (
-                (not test)
-                and rb.idx > min_rb_size
-                and steps_since_train > env_steps_before_train
+                rb.idx > REPLAY_START_SIZE
+                and steps_since_train > 4000
             ):
                 loss = train_step(
-                    m, rb.sample(sample_size), tgt, env.action_space.n, device
+                    m, rb.sample(MINIBATCH_SIZE), tgt, env.action_space.n, device
                 )
-                if step_num % 10 == 0:
-                    print(f"""loss: {loss.detach().cpu().item()},eps: {eps},avg_reward: {np.mean(episode_rewards)}""")
-                episode_rewards = []
+                if epochs_since_tgt % 10 == 0:
+                    print(f"""loss: {loss.detach().cpu().item()},eps: {eps},avg_reward: {episode_rewards}""")
+                    episode_rewards = []
                 epochs_since_tgt += 1
-                epochs_since_test += 1
 
-                if epochs_since_test > epochs_before_test:
-                    rew, frames = run_test_episode(m, test_env, device)
-                    epochs_since_test = 0
-
-                if epochs_since_tgt > tgt_model_update:
+                if epochs_since_tgt > 100:
                     print("updating target model")
                     update_tgt_model(m, tgt)
                     epochs_since_tgt = 0
